fd48afdba1e96b2294852a40d68c01f0d022c1d4
erlhuang <erlhuang@ucsc.edu>
2019-11-03 23:33:41 -0800

    Finished firebase logins/signups, includes sign out button. Will add more to the main screen and login screen.

--- app/src/main/java/dev/lifeStyleRPG/LoginScreen.java
+++ app/src/main/java/dev/lifeStyleRPG/LoginScreen.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -14,43 +15,68 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginScreen extends AppCompatActivity {
+
     EditText emailId, password;
-    Button btnSignUp;
-    FirebaseAuth mFireBaseAuth;
+    Button btnSignIn;
+    TextView tvSignUp;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_screen);
+        setContentView(R.layout.activity_login_screen2);
 
-        mFireBaseAuth = FirebaseAuth.getInstance();
-        emailId = findViewById(R.id.editText);
-        password = findViewById(R.id.editText2);
-        btnSignUp = findViewById(R.id.button);
-        btnSignUp.setOnClickListener(new View.OnClickListener() {
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        emailId = findViewById(R.id.editText4);
+        password = findViewById(R.id.editText5);
+        btnSignIn = findViewById(R.id.button2);
+        tvSignUp = findViewById(R.id.textView);
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+                if( mFirebaseUser != null ){
+                    Toast.makeText(LoginScreen.this,"You are logged in",Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(LoginScreen.this, MapsActivity.class);
+                    startActivity(i);
+                }
+                else{
+                    Toast.makeText(LoginScreen.this,"Please Login",Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = emailId.getText().toString();
                 String pwd = password.getText().toString();
                 if(email.isEmpty()){
-                    emailId.setError("Provide an email");
+                    emailId.setError("Please enter email id");
                     emailId.requestFocus();
                 }
-                else if(pwd.isEmpty()){
+                else  if(pwd.isEmpty()){
                     password.setError("Please enter your password");
                     password.requestFocus();
                 }
+                else  if(email.isEmpty() && pwd.isEmpty()){
+                    Toast.makeText(LoginScreen.this,"Fields Are Empty!",Toast.LENGTH_SHORT).show();
+                }
                 else  if(!(email.isEmpty() && pwd.isEmpty())){
-                    mFireBaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(LoginScreen.this, new OnCompleteListener<AuthResult>() {
+                    mFirebaseAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(LoginScreen.this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if(!task.isSuccessful()){
-                                Toast.makeText(LoginScreen.this,"SignUp Unsuccessful, Please Try Again",Toast.LENGTH_SHORT).show();
+                                Toast.makeText(LoginScreen.this,"Login Error, Please Login Again",Toast.LENGTH_SHORT).show();
                             }
-                            else {
-                                startActivity(new Intent(LoginScreen.this, MapsActivity.class));
+                            else{
+                                Intent intToHome = new Intent(LoginScreen.this, MainActivity.class);
+                                startActivity(intToHome);
                             }
                         }
                     });
@@ -60,10 +86,21 @@ public class LoginScreen extends AppCompatActivity {
 
                 }
 
-
-
             }
         });
 
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intSignUp = new Intent(LoginScreen.this, MainActivity.class);
+                startActivity(intSignUp);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
     }
 }
--- app/src/main/java/dev/lifeStyleRPG/MainActivity.java
+++ app/src/main/java/dev/lifeStyleRPG/MainActivity.java
@@ -20,13 +20,20 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        Button btn = (Button)findViewById(R.id.loginButton);
-
+        Button btn = (Button)findViewById(R.id.signupButton);
+        Button loginbtn = (Button)findViewById(R.id.loginbtn);
         FirebaseUser currentUser = mAuth.getCurrentUser();
         btn.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            startActivity(new Intent(MainActivity.this, LoginScreen.class));
+            startActivity(new Intent(MainActivity.this, RegisterScreen.class));
+            }
+        });
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginScreen.class));
             }
         });
 
@@ -38,9 +45,27 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        if (currentUser == null) {
-            Button btn = (Button)findViewById(R.id.loginButton);
-            btn.setText("Not Logged in!");
+        if(currentUser != null){
+            Button loginbtn = (Button)findViewById(R.id.loginbtn);
+            //turn login button to logout
+            loginbtn.setText("Log Out");
+            loginbtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    FirebaseAuth.getInstance().signOut();
+                    Intent intToMain = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(intToMain);
+                }
+            });
+            Button startBtn = (Button)findViewById(R.id.signupButton);
+            //turn signup button to start
+            startBtn.setText("Play!");
+            startBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(MainActivity.this, MapsActivity.class));
+                }
+            });
         }
     }
 }
