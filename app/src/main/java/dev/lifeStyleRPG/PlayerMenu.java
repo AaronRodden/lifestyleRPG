5f8fc7b437abf40bf33915f7619684cc7b40873a
erlhuang <erlhuang@ucsc.edu>
2019-11-22 14:42:25 -0800

    Fixed some issues in adding users to db, a playermenu screen is now available

--- app/src/main/java/dev/lifeStyleRPG/PlayerMenu.java
+++ app/src/main/java/dev/lifeStyleRPG/PlayerMenu.java
@@ -1,15 +1,61 @@
 package dev.lifeStyleRPG;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class PlayerMenu extends AppCompatActivity {
+    FirebaseFirestore fStore;
+    FirebaseAuth mFireBaseAuth;
+    TextView mainmenu;
+    TextView exptext;
+    TextView trailsdone;
+
+    private static final String TAG = "PlayerMenu";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_menu);
+
+        fStore = FirebaseFirestore.getInstance();
+        mFireBaseAuth = FirebaseAuth.getInstance();
+        String userID = mFireBaseAuth.getCurrentUser().getUid();
+        mainmenu = findViewById(R.id.textView);
+        exptext = findViewById(R.id.totalEXP);
+        trailsdone = findViewById(R.id.trailsDone);
+        DocumentReference docRef = fStore.collection("users").document(userID);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    StringBuilder fields = new StringBuilder("");
+                    fields.append(document.get("login"));
+                    String theuser = fields.toString();
+                    StringBuilder exp = new StringBuilder("Total EXP: \n");
+                    StringBuilder trailstxt = new StringBuilder("Trails Failed: \n");
+                    trailstxt.append(document.get("trails failed").toString());
+                    exp.append(document.get("experience").toString());
+                    exptext.setText(exp.toString());
+                    trailsdone.setText(trailstxt.toString());
+                    mainmenu.setText(theuser);
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+
 
 
     }
--- app/src/main/java/dev/lifeStyleRPG/RegisterScreen.java
+++ app/src/main/java/dev/lifeStyleRPG/RegisterScreen.java
@@ -2,7 +2,6 @@ package dev.lifeStyleRPG;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,20 +11,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
-//sean
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import io.opencensus.tags.Tag;
+//sean
 
 public class RegisterScreen extends AppCompatActivity {
     EditText emailId, password;
@@ -72,13 +66,14 @@ public class RegisterScreen extends AppCompatActivity {
 
                                 Map<String, Object> user = new HashMap<>();
                                 user.put("login", email);
-                                user.put("password", pwd);
+                                user.put("password", "nicetrynoob");
                                 user.put("spriteID", 0);
                                 user.put("level", 1);
                                 user.put("experience", 0);
                                 user.put("trails failed", 0);
+                                user.put("userid", userID);
 
-                                fStore.collection("users").add(user);
+                                fStore.collection("users").document(userID).set(user);
 
                                 startActivity(new Intent(RegisterScreen.this, MainActivity.class));
                             }
